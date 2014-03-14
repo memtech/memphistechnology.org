191b5390b6a46236b99f70c5f8c034aef0ad11b7
svpernova09 <halo09@gmail.com>
2014-03-14 11:05:19 -0500

    ENGAGE...i mean DEPLOY!

--- publish.sh
+++ publish.sh
@@ -7,5 +7,5 @@
 sculpin generate --env=prod
 if [ $? -ne 0 ]; then echo "Could not generate the site"; exit 1; fi
 
-rsync -avze 'ssh -p 4668' output_prod/ username@yoursculpinsite:public_html
+rsync -avze 'ssh -p 22' output_prod/ memphis@midsouthmakers.org:public_html
 if [ $? -ne 0 ]; then echo "Could not publish the site"; exit 1; fi
