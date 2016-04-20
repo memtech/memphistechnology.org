3ef488ff1539ee28960f47d093a355efd39bc6fe
Joe Ferguson <joe@joeferguson.me>
2016-04-19 21:24:56 -0500

    purge local generated files before starting publish or watch command

--- publish.sh
+++ publish.sh
@@ -3,7 +3,7 @@
 # Replace "sculpin generate" with "php sculpin.phar generate" if sculpin.phar
 # was downloaded and placed in this directory instead of sculpin having been
 # installed globally.
-
+rm -rf output_prod
 sculpin generate --env=prod
 if [ $? -ne 0 ]; then echo "Could not generate the site"; exit 1; fi
 
--- watch.sh
+++ watch.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
+rm -rf output_dev
 sculpin generate --watch --server
