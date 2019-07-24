aded593b9ff002b93ccc32322375db5dafb88f7d
Joe Ferguson <joe@joeferguson.me>
2019-07-24 14:17:11 -0500

    use local sculpin to build, +x publish

--- publish.sh
+++ publish.sh
@@ -4,7 +4,7 @@
 # was downloaded and placed in this directory instead of sculpin having been
 # installed globally.
 rm -rf output_prod
-sculpin generate --env=prod
+./vendor/bin/sculpin generate --env=prod
 if [ $? -ne 0 ]; then echo "Could not generate the site"; exit 1; fi
 
 rsync -avze 'ssh -p 22' output_prod/ memphistechnology@memphistechnology.org:public_html
